f6c2ffe672d3ed2e6e007a3e666b7b9d47838f3b
RiickNogueira <richardsonogueira@gmail.com>
2017-06-20 09:40:32 -0300

    delete env.js

--- src/env.js
+++
@@ -1,8 +0,0 @@
-export const config_firebase = {
-    apiKey: "AIzaSyBMDujyQop-1c-4NHRHAOuB20f50v2lP1o",
-    authDomain: "del-lanches.firebaseapp.com",
-    databaseURL: "https://del-lanches.firebaseio.com",
-    projectId: "del-lanches",
-    storageBucket: "del-lanches.appspot.com",
-    messagingSenderId: "582899881395"
-}
